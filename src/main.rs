e6347073efa9c3f1b0eac4e21444a61d26de0668
StackDoubleFlow <ojaslandge@gmail.com>
2019-12-12 20:27:13 -0600

    Getting Devices

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+
+use std::fs::{read_dir};
+
+fn get_devices() -> Vec<String> {
+    read_dir("/sys/class/net/").unwrap().map(|res| res.unwrap().file_name().into_string().unwrap()).collect()
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    println!("{:?}", get_devices());
 }

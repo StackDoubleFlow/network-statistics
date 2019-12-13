d6e6cd4a14e1fc7b4092e902214f57e44cdfe3b9
StackDoubleFlow <ojaslandge@gmail.com>
2019-12-12 21:42:41 -0600

    Print statistics

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,20 @@
 
-use std::fs::{read_dir};
+use std::fs::{read_dir, read_to_string};
 
 fn get_devices() -> Vec<String> {
     read_dir("/sys/class/net/").unwrap().map(|res| res.unwrap().file_name().into_string().unwrap()).collect()
 }
 
+fn read_entry(device: &String, entry: &str) -> String {
+    read_to_string(format!("/sys/class/net/{}/statistics/{}", device, entry)).unwrap()
+}
 
 fn main() {
-    println!("{:?}", get_devices());
+    for device in get_devices() {
+        println!("Printing statistics for device \"{}\":", device);
+        print!("Total Bytes Sent - {}", read_entry(&device, "tx_bytes"));
+        print!("Total Bytes Received - {}", read_entry(&device, "rx_bytes"));
+        print!("Total Packets Sent - {}", read_entry(&device, "tx_packets"));
+        println!("Total Packets Received - {}", read_entry(&device, "rx_packets"));
+    }
 }
